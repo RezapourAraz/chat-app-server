816bdfdf41c4bd70e3013e3bc4b85af4340c5053
ArazRezapour <rezapouraraz@gmail.com>
2023-06-08 09:22:52 +0330

    working on socket io

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+//https://chat-app-server.iran.liara.run/
+
 // routes
 const userRouter = require("./routes/userRoute");
 
@@ -17,12 +19,12 @@ app.use(express.json());
 app.use("/auth", userRouter);
 
 io.on("connection", (socket) => {
-  console.log("a user connected");
+  console.log(`⚡: ${socket.id} user just connected!`);
   socket.on("chat message", (msg) => {
     io.emit("chat message", msg);
   });
   socket.on("disconnect", () => {
-    console.log("user disconnect");
+    console.log("🔥: A user disconnected");
   });
 });
 
